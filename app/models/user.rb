b366324d63a8d4d7c784245e70f0980e47b894b4
ryotakebayashi <rrr.7164.rrr@gmail.com>
2018-09-25 12:00:39 +0900

    posts commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  def change
+    
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   get "about" => "home#about"
   #業務内容ページ
   get "contents" => "home#contents"
-
+ resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
